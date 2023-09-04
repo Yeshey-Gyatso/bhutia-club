87bb58a2938fc9dc656660e900acd46e896e4d90
Yeshey <yesheygyatsoo.ygu@gmail.com>
2023-09-04 15:39:54 +0530

    installed tw-elements

--- src/app/components/landing1.tsx
+++ src/app/components/landing1.tsx
@@ -1,6 +1,13 @@
 "use client"
 
 import React from 'react'
+// Initialization for ES Users
+import {
+    Carousel,
+    initTE,
+  } from "tw-elements";
+  
+  initTE({ Carousel });
 
 const Landing1 = () => {
   return (
