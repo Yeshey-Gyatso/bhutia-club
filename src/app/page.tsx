a36033820f4408d71e8613d16c712e721489c2eb
Yeshey <yesheygyatsoo.ygu@gmail.com>
2023-09-04 15:40:19 +0530

    added - new package tw-elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import Image from 'next/image'
 import Footer from './components/footer'
+import Landing1 from './components/landing1'
+import Landing2 from './components/landing2'
+import Landing3 from './components/landing3'
 
 export default function Home() {
   return (<div>
+    <Landing1/>
+    <Landing2/>
+    <Landing3/>
     <Footer/>
   </div>
   )
