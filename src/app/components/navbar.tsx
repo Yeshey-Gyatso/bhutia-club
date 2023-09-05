ebc1382204252ab78bed74a816835af58a1b0d8f
Yeshey <yesheygyatsoo.ygu@gmail.com>
2023-09-05 12:35:36 +0530

    making page selector

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,9 +1,18 @@
+
 import React from 'react'
 
 const Navbar = () => {
   return (
     <div>
-        <div className='fixed p-10   h-20 w-full bg-black '></div>
+        <div className='fixed p-2 h-16 w-full bg-orange-200 z-10 '>
+            <div className=' justify-stretch'>
+            <h1>hello</h1>
+            </div>
+            <div className=''>
+           
+            </div>
+            
+        </div>
     </div>
   )
 }
