b97df8807f759ddcf4b56d8ada3f3aaf607c3975
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-09-06 10:28:36 +0530

    updating navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -11,17 +11,79 @@ import { toast } from 'react-toastify';
 
 const Navbar = () => {
   return (
-    <div>
-        <div className='fixed p-2 h-16 w-full bg-orange-200 z-10 '>
-            <div className=' justify-stretch'>
-            <h1>hello</h1>
-            </div>
-            <div className=''>
-           
-            </div>
-            
-        </div>
-    </div>
+    return (
+      <div className=' flex md:justify-between 
+      bg-emerald-950 
+       w-full 
+       fixed
+        p-4
+         z-10'>
+          <div className=' brand'>
+              <h1 className='md:text-xl font-bold border-b-2 border-white
+                       md:p-1 inline cursor-default '>Vintage Melodies</h1>
+          </div>
+          <div className=' flex space-x-2  '>
+              <h1><Link href={'/'} className=' hover:text-gray-300'>Home</Link></h1>
+              {
+                context.user &&(
+                  <>
+              <h1><Link href={'/courses' }className=' hover:text-gray-300'>Courses</Link></h1>
+              <h1><Link href={'/profile/user' }className=' hover:text-gray-300'> Purchases</Link></h1>   
+                  </>
+  
+                )
+              }
+              </div>
+          <div>
+              <ul className='flex   space-x-2 '>
+              {
+                context.user && (
+                  <>
+                     <li>
+                 <h1 className=' inline cursor-default'
+                  
+                 >{context.user.username}</h1>
+                  </li>
+                  <li>
+                    <button
+                    onClick={doLogout} 
+                    >logout</button>
+                  </li>
+  
+                  </>
+                )
+              }
+              {
+                !context.user &&(
+                  <>
+                    <li>
+                 <Link href={'/login'} 
+                 >login</Link>
+                  </li>
+                  <li>
+                    <Link href={'/signupUser'}
+                     
+                    >Signup</Link>
+                  
+                  </li>
+                  </>
+                )
+              }
+                 
+              </ul>
+                    
+                
+                     
+                     
+                  
+              
+                  
+          </div>
+          </div>
+    )
+  }
+  
+  export default CustomNavbar;
   )
 }
 
