e8d6ae5f660c45fb053cfb0a92479b3c98792518
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-09-09 15:36:17 +0530

    changing pages

--- src/app/components/page1.tsx
+++ src/app/components/page1.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Page1 = () => {
   return (
-    <div className=' h-screen w-full bg-gradient-to-b from-white via-yellow-200 to-yellow-400'>Page1</div>
+    <div className=' h-screen w-full bg-gradient-to-b from-white to-teal-400'>Page1</div>
   )
 }
 
