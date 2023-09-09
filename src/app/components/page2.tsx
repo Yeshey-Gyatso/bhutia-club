8b63055072d296bf926cfc04f476bbea99256c09
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-09-09 11:59:25 +0530

    edited page 1 and 2

--- src/app/components/page2.tsx
+++ src/app/components/page2.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Page2 = () => {
   return (
-    <div className=' h-screen w-screen bg-gradient-to-b from-yellow-500 to-yellow-50'>Page1</div>
+    <div className=' h-screen w-full bg-gradient-to-b from-yellow-400 to-yellow-600'>Page1</div>
     )
 }
 
