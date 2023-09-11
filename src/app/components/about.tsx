6c6b8c891b70b0e7550898d8dc01d63431d4e8a7
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-09-11 12:04:03 +0530

    adding about title and para

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -5,7 +5,14 @@ const About = () => {
     <div className='
     h-screen w-full
     bg-gradient-to-b from-white via-blue-100 to-sky-500'>
-      About of Makhim
+      <div className='pl-10 pt-28 flex flex-col'>
+            <h1 className=' text-7xl'>WHO WE ARE </h1>
+            <h1 className=' text-3xl'> Based on Gangtok Sikkim </h1>
+      </div>
+      <div
+      >
+
+      </div>
     </div>
   )
 }
