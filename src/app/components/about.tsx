40cc82d81ba8bdb9b58b9afe37fdeec02bf1cfbf
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-09-11 14:06:41 +0530

    about me full complete

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -4,19 +4,29 @@ import logoT from '../public/logoT.png';
 const About = () => {
   return (
     <div className='
+    
     h-screen w-full
     bg-gradient-to-b from-white via-blue-100 to-sky-500'>
-      <div className='pl-10 pt-28 flex flex-col'>
-            <h1 className=' text-6xl font-extrabold'>WHO WE ARE </h1>
-            <h1 className=' ml-28 py-4 text-2xl font-bold'> Based in Gangtok with team from all over Sikkim to ... ?
-              </h1>
+      <div className='pl-10 pt-20 flex flex-col'>
+            <h1 className=' text-6xl font-extrabold border-b-8'>WHO WE ARE </h1>
+            <h1 className=' ml-6 py-8 text-2xl font-bold'> Based in Gangtok with team from all over Sikkim to ... ? </h1>
+            
             </div>
       <div className=' flex w-full relative '>
-          <div className=' inset-x-1/2 absolute w-40 bg-red-600'>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          <div className=' inset-x-1/2 absolute w-1/3'>
+              <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                 Unde distinctio omnis sequi quaerat voluptates aut
                 harum mollitia soluta fugit eum atque, 
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, magnam vel maiores necessitatibus, optio sed commodi voluptates consequatur, iste incidunt voluptate soluta 
+                saepe deleniti molestias cupiditate facilis impedit 
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus nulla harum sit velit. In non quia eius placeat aperiam atque illo fuga? Omnis velit laudantium quasi ullam sit saepe similique.
+                numquam minus?
                ipsa velit ea non voluptatem facere explicabo laboriosam cum.</p>
+                <br /><br />
+               <p>
+                Lorem ipsum d Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda amet quisquam beatae voluptatum, facere ab deserunt porro accusamus id labore consequuntur placeat vitae sint eaque! Itaque a expedita repellat nemo?
+                olor sit amet, consectetur adipisicing elit. Mollitia possimus amet repellat est sapiente suscipit quod, voluptatem minima odio alias accusamus numquam consectetur quibusdam praesentium aliquid, adipisci fugiat enim nostrum.
+               </p>
              </div>
           
       </div>
@@ -29,6 +39,7 @@ const About = () => {
               height={1000}
             />
             </div>
+
     </div>
   )
 }
