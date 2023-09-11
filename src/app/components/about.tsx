7996d915e9f1955a3b7382aeee87bfe03acc115b
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-09-11 12:22:26 +0530

    adding para 1 and image

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -6,12 +6,13 @@ const About = () => {
     h-screen w-full
     bg-gradient-to-b from-white via-blue-100 to-sky-500'>
       <div className='pl-10 pt-28 flex flex-col'>
-            <h1 className=' text-8xl font-extrabold'>WHO WE ARE </h1>
-            <h1 className=' ml-60 py-4 text-3xl font-bold'> Based in Gangtok Sikkim </h1>
-      </div>
-      <div
-      >
-
+            <h1 className=' text-6xl font-extrabold'>WHO WE ARE </h1>
+            <h1 className=' ml-28 py-4 text-2xl font-bold'> Based in Gangtok with team from all over Sikkim to ... ?
+              </h1>
+            </div>
+      <div className=' flex flex-row '>
+          <div className=' bg-red-600'> para 1</div>
+          <div className=' bg-yellow-600'> image</div>
       </div>
     </div>
   )
