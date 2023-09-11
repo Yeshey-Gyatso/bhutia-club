8a8ffddd55d7b242f5633bb5632b688fa28b9797
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-09-11 12:50:50 +0530

    trying again !

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from 'react'
 import Image from 'next/image';
 import logoT from '../public/logoT.png';
 const About = () => {
   return (
     <div className='
-    
     h-screen w-full
     bg-gradient-to-b from-white via-blue-100 to-sky-500'>
       <div className='pl-10 pt-28 flex flex-col'>
@@ -12,25 +11,20 @@ const About = () => {
             <h1 className=' ml-28 py-4 text-2xl font-bold'> Based in Gangtok with team from all over Sikkim to ... ?
               </h1>
             </div>
-        {/* //div to compress the contents */}
-        <div className='relative flex bg-slate-600 w-4/5 h-auto'>
-            <div className=' flex flex-row justify-center items-center '>
-                  <div className=' bg-red-600 '>  <Image 
-                          src={logoT} 
-                             alt='pic of logo'
-                            width={500}
-                            height={500}
-                          /></div>
-                        <div className='w-auto h-auto
-                        bg-yellow-600 flex flex-col justify-center 
-                        items-center text-center'>
-                          <p className="text-left tracking-wide text-lg"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt nobis quae facilis a quaerat eaque esse! Maxime libero aperiam nostrum 
-                            debitis provident facilis sequi suscipit quo pariatur, similique, quidem error!</p>
-                      
-                          </div>
-                    </div>
-          </div>
-      
+      <div className=' flex flex-row '>
+          <div className=' bg-red-600'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde distinctio omnis sequi quaerat voluptates aut
+           harum mollitia soluta fugit eum atque, 
+           ipsa velit ea non voluptatem facere explicabo laboriosam cum.</div>
+          <div className=' h-100 w-100 bg-yellow-600'>
+            
+          <Image 
+              src={logoT} 
+              alt='pic of logo'
+              width={60}
+              height={60}
+            />
+            </div>
+      </div>
     </div>
   )
 }
