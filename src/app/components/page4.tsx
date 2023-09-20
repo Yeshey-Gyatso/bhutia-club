4cdfe8b5ccec6b09beab349dd68ed6c48bd0edee
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-09-20 11:51:56 +0530

    completed adding divider

--- src/app/components/page4.tsx
+++ src/app/components/page4.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 const Page4 = () => {
   return (
     <div className='
+     bg-gradient-to-br from-white to-gray-500
     flex justify-center items-center
-        h-full w-screen
-    '><h1 className=' text-6xl '>
-        DIFFERENT SSTYLE BELOW
+        h-screen w-screen
+    '><h1 className='  md:text-6xl '>
+        DIFFERENT STYLE BELOW
         </h1>
         </div>
   )
