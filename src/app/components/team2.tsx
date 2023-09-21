a675448ff01b72fc91ec624d5f9eb212cd6c7e3b
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-09-21 06:52:53 +0530

    some minor tweaks

--- src/app/components/team2.tsx
+++ src/app/components/team2.tsx
@@ -3,7 +3,7 @@
 import React from 'react'
 
 const Team2 = () => {
-  return (<section className=" bg-white dark:bg-gray-900">
+  return (<section className=" bg-gradient-to-b from-amber-300 to-gray-200   dark:bg-gray-900">
   <div className=" py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
       <div className=" mx-auto mb-8 max-w-screen-sm lg:mb-16">
           <h2 className=" mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our team</h2>
