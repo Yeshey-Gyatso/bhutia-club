d7c3bc24a5ad62fbd978ddcd11941e696254a5fb
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-09-23 19:00:27 +0530

    added animation on extra page

--- src/app/components/page1.tsx
+++ src/app/components/page1.tsx
@@ -1,13 +1,13 @@
 import React, { useRef } from 'react'
 import Paragraph from './paragraph'
-import { useScroll, useTransform } from 'framer-motion';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 const Page1 = () => {
   const ref = useRef<HTMLDivElement>(null);
     
   const {scrollYProgress}= useScroll({target:ref})
   
-  const x = useTransform(scrollYProgress, [-1, 0, 1], [-100, 100, -500]);
+  const x = useTransform(scrollYProgress, [-1, 0, 1], [+500, 0, 500]);
 
   return (
     <section  
@@ -35,13 +35,18 @@ const Page1 = () => {
         
         '>
             
-              <div 
-                className='relative z-10 flex flex-col items-center bottom-0 mt-40'>
+              <motion.div 
+
+              ref={ref}
+              initial={{ x: -100 }} style={{x}}
+                className='relative flex flex-col items-center bottom-0 mt-40'>
                 <div className='relative flex flex-row items-center'>
                 <div className=' 
                      grid-row-auto
                      my-10  py-10  bg-gray-800 opacity-70 w-full px-8 flex flex-col items-center '>
-                        <div className=' cursor-default py-4 text-white md:text-2xl max-w-full sm:mx-auto z-10 font-light   
+                        <div
+                        
+                        className=' cursor-default py-4 text-white md:text-2xl max-w-full sm:mx-auto z-10 font-light   
                         '>
                             
                           "Welcome to our literary club, where words come alive and stories unfold!"
@@ -52,7 +57,7 @@ const Page1 = () => {
                 
                
                 </div> 
-                </div> 
+                </motion.div> 
         </div>
         
     </section>
