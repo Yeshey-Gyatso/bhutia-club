975a228acad814d6bde0ce9df7a74f614f313e0e
Yeshey <yesheygyatsoo.ygu@gmail.com>
2023-11-06 11:48:12 +0530

    removing unwated things from blog

--- src/app/blog/blogcard.tsx
+++ src/app/blog/blogcard.tsx
@@ -12,7 +12,7 @@ const BlogCard = () => {
     ]
   return (
     <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <Link href="https://bhutiakhayrabyargaytshogpo.blogspot.com/" as="/blog/noteworthy-technology-acquisitions-2021">
+      <Link href="https://bhutiakhayrabyargaytshogpo.blogspot.com/" >
         <div className="hover:underline">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Bhutia Khayrab Yargay Tshogpo</h5>
         </div>
