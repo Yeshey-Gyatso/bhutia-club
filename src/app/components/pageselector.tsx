c7fa3fcab4fbb212ac7a4f62d6187cd3e8e76193
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-11-21 07:34:50 +0530

    minor changes in landing

--- src/app/components/pageselector.tsx
+++ src/app/components/pageselector.tsx
@@ -2,20 +2,11 @@
 import React, { useState } from 'react'
 import Landing1 from './landing1'
 import Navbar from './navbar'
-import Landing2 from './landing2'
-import Landing3 from './landing3'
-import Footer from './footer'
 import { RecoilRoot } from 'recoil'
 import Page1 from './page1'
-import Page2 from './page2'
-
 import Team from './team'
-
 import About from './about'
 import Page4 from './page4'
-import About2 from './about2'
-import Team2 from './team2'
-import AboutPage from '../aboutPage/aboutPage'
 import Contact from './contactUs'
 const PageSelector = () => {
   const [landing,setLanding]=useState(0);
