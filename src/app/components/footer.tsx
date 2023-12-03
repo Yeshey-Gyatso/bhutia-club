7daac22b8026deb30c37bd546cbee694107a45d0
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-12-03 21:52:06 +0530

    adding some footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -5,8 +5,8 @@ const Footer = () => {
   return (<footer className=' bottom-0 left-0 z-50  bg-gray-500  w-full lg:mx-auto sm:max-w-screen-x'>
     <div className=' flex md:p-5 justify-around'>
       <div className=' text-left md:text-center flex flex-col justify-center '>
-          <h1 className='text-lg md:text-3xl '>Welcome to JamStore</h1>
-          <p className=' text-xs md:text-base pt-4'>A music store friendly from beginner to pro 
+          <h1 className='text-lg md:text-3xl '>Bhutia Khayrab Yargay Tshogpo</h1>
+          <p className=' text-xs md:text-base pt-4'>The official Bhutia literary club of Sikkim 
              .</p>
       </div>
       
@@ -14,7 +14,7 @@ const Footer = () => {
       <h1>Import Links</h1>
       <ul>
         <li><a href="#!">facebook</a></li>
-        <li><a href="#!">Instagram</a></li>
+        <li><a href="#!">Youtube</a></li>
         <li><a href="#!">LinkedIn</a></li>
       </ul>
     </div>
