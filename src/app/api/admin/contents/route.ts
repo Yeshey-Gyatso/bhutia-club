41a1baf41b172957a041669ea20cba4f1f7cc9e7
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-12-06 16:04:55 +0530

    finally api worked

--- src/app/api/admin/contents/route.ts
+++ src/app/api/admin/contents/route.ts
@@ -1,20 +1,21 @@
 
 import fs from 'fs';
+import { NextResponse } from 'next/server';
 import { join } from 'path';
 
-export default function handler(req, res) {
+export async function GET(req:Request, res:NextResponse) {
   if (req.method === 'GET') {
     const dataDirectory = join(process.cwd(), 'about.json');
     const rawData = fs.readFileSync(dataDirectory);
     const data = JSON.parse(rawData);
-    res.status(200).json(data);
-  } else if (req.method === 'POST') {
-    const newData = req.body;
-    const stringifiedData = JSON.stringify(newData, null, 2);
-    const dataDirectory = join(process.cwd(), 'about.json');
-    fs.writeFileSync(dataDirectory, stringifiedData);
-    res.status(200).json(newData);
+    return NextResponse.json(data)
+//   } else if (req.method === 'POST') {
+//     const newData = req.body;
+//     const stringifiedData = JSON.stringify(newData, null, 2);
+//     const dataDirectory = join(process.cwd(), 'about.json');
+//     fs.writeFileSync(dataDirectory, stringifiedData);
+//     res.status(200).json(newData);
   } else {
-    res.status(405).end(); // Method Not Allowed
+   return NextResponse.error(); // Method Not Allowed
   }
 }
