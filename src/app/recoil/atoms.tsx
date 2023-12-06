6abdf7eef999850fb0a4a7a1b89db5b812a95af7
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-12-06 11:36:39 +0530

    adding persistatom

--- src/app/recoil/atoms.tsx
+++ src/app/recoil/atoms.tsx
@@ -1,23 +1,10 @@
 import { atom, RecoilState, SetterOrUpdater } from "recoil";
-
-const localStorageEffect = (key: string) => ({ setSelf, onSet }: { setSelf: SetterOrUpdater<any>; onSet: (newValue: any) => void }) => {
-  // Check if localStorage is available
-  if (typeof localStorage !== 'undefined') {
-    const savedValue = localStorage.getItem(key);
-
-    if (savedValue != null) {
-      setSelf(JSON.parse(savedValue));
-    }
-
-    onSet((newValue: any) => {
-      localStorage.setItem(key, JSON.stringify(newValue));
-    });
-  }
-};
-
+import { recoilPersist } from 'recoil-persist';
+const { persistAtom } = recoilPersist()
 export const languageAtom = atom({
   key: "language",
+ 
   default: false,
-  effects_UNSTABLE: [localStorageEffect('language')],
+  effects_UNSTABLE: [persistAtom],
 });
 ``
