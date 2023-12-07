b7f7b4ecf54d4bc91233ca6a11d846e50b7a725c
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-12-07 11:48:05 +0530

    put also finished

--- src/app/api/admin/contents/route.ts
+++ src/app/api/admin/contents/route.ts
@@ -44,3 +44,44 @@ export async function POST(req: Request, res: NextResponse) {
     return NextResponse.error();
   }
 }
+
+
+
+export async function PUT(req: Request, res: NextResponse) {
+  if (req.method === 'PUT') {
+    try {
+      const { title, description } = await req.json();
+
+      const dataDirectory = join(process.cwd(), 'about.json');
+
+      // Check if the file exists
+      if (!fs.existsSync(dataDirectory)) {
+        return NextResponse.json({
+          message: 'Resource not found',
+          status: false,
+        });
+      }
+
+      // Read existing data from the file
+      const existingData = JSON.parse(fs.readFileSync(dataDirectory, 'utf-8'));
+
+      // Update the existing data with new values
+      existingData.title = title;
+      existingData.description = description;
+
+      // Write the updated data back to the file
+      fs.writeFileSync(dataDirectory, JSON.stringify(existingData, null, 2));
+
+      // Return the updated data or any other response as needed
+      return NextResponse.json(existingData, { status: 200 });
+    } catch (error) {
+      console.error(error);
+      return NextResponse.json({
+        message: 'Failed to update resource',
+        status: false,
+      });
+    }
+  } else {
+    return NextResponse.error();
+  }
+}
