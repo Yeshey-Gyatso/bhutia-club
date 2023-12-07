ebf11316c313e0ca384521e973ac355dc5c71733
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-12-07 11:31:40 +0530

    trying the post medthod

--- src/app/api/admin/contents/route.ts
+++ src/app/api/admin/contents/route.ts
@@ -9,13 +9,20 @@ export async function GET(req:Request, res:NextResponse) {
     const rawData = fs.readFileSync(dataDirectory);
     const data = JSON.parse(rawData);
     return NextResponse.json(data)
-//   } else if (req.method === 'POST') {
-//     const newData = req.body;
-//     const stringifiedData = JSON.stringify(newData, null, 2);
-//     const dataDirectory = join(process.cwd(), 'about.json');
-//     fs.writeFileSync(dataDirectory, stringifiedData);
-//     res.status(200).json(newData);
   } else {
    return NextResponse.error(); // Method Not Allowed
+  }
+}
+
+
+export async function POST(req:Request, res:NextResponse) {
+  if (req.method === 'POST') {
+    const newData = req.body;
+    const stringifiedData = JSON.stringify(newData, null, 2);
+    const dataDirectory = join(process.cwd(), 'about.json');
+    fs.writeFileSync(dataDirectory, stringifiedData);
+        return NextResponse.json(newData);
+  } else {
+   return NextResponse.error(); 
   }
 }
