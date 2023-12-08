e6c82c152b7ed1f7dd9c75c45bc649af555a1f10
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-12-08 07:49:39 +0530

    connected json file to about

--- src/app/about/aboutPage.tsx
+++ src/app/about/aboutPage.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+
 import logoT from '../public/logoT.png';
 import Image from 'next/image'
 import Link from 'next/link';
@@ -6,8 +7,37 @@ import Navbar from '../components/navbar';
 import { useRecoilValue } from 'recoil';
 import { languageAtom } from '../recoil/atoms';
 import { Heading1 } from 'lucide-react';
+import React, { useEffect, useState } from 'react';
+
 const About = () => {
   const language=useRecoilValue(languageAtom);
+  const [aboutData, setAboutData] = useState({ title1: '', description1: '',title2: '', description2: '',
+  btitle1: '', bdescription1: '',btitle2: '', bdescription2: '',
+});
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch('/api/admin/contents', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Request failed with status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setAboutData(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      // Handle the error appropriately
+    }
+  };
+
+  fetchData();
+}, []); 
   return (
     
     <div className=' h-screen w-screen flex justify-center '>
@@ -33,15 +63,15 @@ const About = () => {
         <section className="max-w-4xl mx-auto p-4 bg-white rounded-lg shadow-md">
           <h1 className="text-lg font-semibold mb-4">
             {
-              language?(<h1>About Us in bhutia</h1>):(<h1>About Us</h1>)
+              language?(<h1>{aboutData.btitle1}</h1>):(<h1>{aboutData.title1}</h1>)
             }
             </h1>
           <div className="text-gray-700 text-sm">
             {
               language ? (
-                  <p>in bhutia</p>
+                  <p>{aboutData.bdescription1}</p>
               ):(
-                  <p> Welcome to Bhutia Khayrab Yargay Tshogpo. We are a dedicated team of professionals passionate about delivering high-quality products and services to our customers.</p>
+                  <p>{aboutData.description1}</p>
               )
             }
            
@@ -50,55 +80,15 @@ const About = () => {
 
           <div className="text-lg font-semibold mt-8 mb-4">
             {
-              language?(<h2>About Denjongke in bhutia</h2>):(<h2>About Denjongke</h2>)
+              language?(<h2>{aboutData.btitle2}</h2>):(<h2>{aboutData.title2}</h2>)
             }
             
             
             </div>
           <div className="text-gray-700 text-sm">
             {
-              language?(<p>bhutia-  Denjongke is a Tibetic (Tournadre 2008: 283) language, spoken in the formerly
-              independent kingdom of Sikkim, which is now a state of India. One of the most
-              closely related languages within the Tibetic family is Dzongkha, the official
-              language of Bhutan. The Census of India 2001 lists 41,825 speakers for
-              Denjongke, whereas a few Denjongke language teachers have given estimates of
-              some 25–30,000 speakers at present. The exact number of speakers is difficult to
-              determine because many young ethnic Denjongpas either do not speak the
-              language or have a very limited knowledge of it. Moreover, many Lepchas
-              living close to Denjongpas are reported to speak Denjongke. Denjongke
-              grammar has been described by Sandberg (1888, 1895). Walsh (1905), Grierson
-              ([1909] 1967) and Shafer (1974) also provide some lexical data and historicalcomparative
-              observations on Denjongke phonology. Sandberg (1895: 44-45)
-              gives a brief list of sentences illustrating Denjongke copulas (which he calls
-              “substantive verbs”) but his description is not very detailed. References to
-              Sandberg (1895), when relevant, are given later in this article.
-              The most common endonym of the language is Lhoke2 ‘southern language’,
-              while Denjongke3 is a pan-Tibetan term better understood by other Tibetanrelated
-              communities. In India, the language is widely known as (Sikkimese)
-              Bhutia, and in the Ethnologue (Lewis et al. 2013) as Sikkimese. The dialects of
-              the Northern villages of Lachen and Lachung are called by the names of these
-              villages.</p>)
-              :(<p>Denjongke is a Tibetic (Tournadre 2008: 283) language, spoken in the formerly
-              independent kingdom of Sikkim, which is now a state of India. One of the most
-              closely related languages within the Tibetic family is Dzongkha, the official
-              language of Bhutan. The Census of India 2001 lists 41,825 speakers for
-              Denjongke, whereas a few Denjongke language teachers have given estimates of
-              some 25–30,000 speakers at present. The exact number of speakers is difficult to
-              determine because many young ethnic Denjongpas either do not speak the
-              language or have a very limited knowledge of it. Moreover, many Lepchas
-              living close to Denjongpas are reported to speak Denjongke. Denjongke
-              grammar has been described by Sandberg (1888, 1895). Walsh (1905), Grierson
-              ([1909] 1967) and Shafer (1974) also provide some lexical data and historicalcomparative
-              observations on Denjongke phonology. Sandberg (1895: 44-45)
-              gives a brief list of sentences illustrating Denjongke copulas (which he calls
-              “substantive verbs”) but his description is not very detailed. References to
-              Sandberg (1895), when relevant, are given later in this article.
-              The most common endonym of the language is Lhoke2 ‘southern language’,
-              while Denjongke3 is a pan-Tibetan term better understood by other Tibetanrelated
-              communities. In India, the language is widely known as (Sikkimese)
-              Bhutia, and in the Ethnologue (Lewis et al. 2013) as Sikkimese. The dialects of
-              the Northern villages of Lachen and Lachung are called by the names of these
-              villages.</p>)
+              language?(<p>{aboutData.bdescription2}</p>)
+              :(<p>{aboutData.description2}</p>)
             }
                       
           </div>
